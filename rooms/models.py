8b933b3101d48f91ea511acdab72fdadb9da24ac
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-11-11 23:57:36 +0300

    updated the database table

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Rooms(models.Model):
@@ -9,6 +10,7 @@ class Rooms(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     amenities = models.JSONField()
     available_today = models.BooleanField(default=True)
+    shared_shower = models.BooleanField(default=False)
     
     class Meta:
         verbose_name = "Room"
@@ -42,8 +44,9 @@ class Reservation(models.Model):
     guests = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=[("confirmed", "Confirmed"), ("pending", "Pending"), ("cancelled", "Cancelled")], default="pending")
-    guest_name = models.CharField(max_length=100)
-    guest_phone = models.CharField(max_length=15)
-
+    guest_email = models.EmailField(max_length=50,default=None)
+    guest_first_name = models.CharField(max_length=100,default=None)
+    guest_last_name = models.CharField(max_length=100,default=None)
+    guest_phone = models.CharField(max_length=15, validators=[RegexValidator(regex=r'^\+?\d{10,15}$', message='Phone number must be entered in the format: "0912345678". Up to 15 digits allowed.')])
     def __str__(self):
         return f"Reservation for Room {self.room.room_num}"
