102c0d98c5fdd854dad63cb167c2e4046121c1f7
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-11-19 14:42:00 +0300

    test uisng unicorn

--- YoniGuestHouseBackend/settings.py
+++ YoniGuestHouseBackend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-62+=^l39etr+v0mwrutn05ymoj_t6b#1-&z0pw21o+gx^*$+qp'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- dockerfile
+++ dockerfile
@@ -6,10 +6,8 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-RUN python manage.py migrate
-
 RUN python manage.py collectstatic --no-input
 
 EXPOSE 8000
 
-CMD ["python", "manage.py", "runserver", "--insecure", "0.0.0.0:8000"]
+CMD ["gunicorn", "--workers=3", "myproject.wsgi:application"]
