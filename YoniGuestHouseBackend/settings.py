846c633186b094855979f9d82d40f0ad757fcb14
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-11-19 12:55:39 +0300

    staticfiles storage fix

--- YoniGuestHouseBackend/settings.py
+++ YoniGuestHouseBackend/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 
@@ -165,4 +165,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
