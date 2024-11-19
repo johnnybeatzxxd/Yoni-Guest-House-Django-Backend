6fb28750e1b4929e91241e91d6b8caa36847b9b4
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-11-19 15:41:01 +0300

    typo

--- YoniGuestHouseBackend/settings.py
+++ YoniGuestHouseBackend/settings.py
@@ -22,11 +22,11 @@ ALLOWED_HOSTS = [".vercel.app","127.0.0.1"]
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://yoniguesthouse.top",
+    "https://www.yoniguesthouse.top",
 
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://yoniguesthouse.top",
+    "https://www.yoniguesthouse.top",
 ]
 
 
